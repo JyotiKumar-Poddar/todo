914870325234054a57311bec19ab68795756d41e
JyotiKumarPoddar <jyoti@skenariolabs.com>
2020-09-28 22:20:30 +0300

    added icons

--- components/toDoItems.js
+++ components/toDoItems.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const ToDoItems = ({ item, onPresHandler }) => {
     return (
         <TouchableOpacity onPress={() => onPresHandler(item.id)}>
-            <Text style={styles.item} >{item.title}</Text>
+            <View style={styles.item}>
+                <Text>{item.title}</Text>
+                <MaterialIcons name="delete" size={24} color="ba" />
+            </View>
         </TouchableOpacity>
     )
 }
@@ -18,6 +22,11 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderStyle: 'dashed',
         borderRadius: 20,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+
     },
+
 })
 export default ToDoItems;
